34b7aaa2f97ca67d12e56ca70d635a62f09935e3
martin0626 <scbsteel12@gmail.com>
2024-09-25 16:52:01 +0300

    Added Form Errors Handler

--- src/components/Booking/BookingForm.jsx
+++ src/components/Booking/BookingForm.jsx
@@ -1,38 +1,134 @@
 import emailjs  from "@emailjs/browser";
 import { useRef, useState } from "react"
 
-export default function BookingForm(){
-    const [selectedInput, setSelectedInput] = useState('');
-    const formRef = useRef();
+
+
+const checkName = (string)=>{
+
+    const [firstName, lastName] = string.trim().split(' ');
+
+
+    if(!firstName || !lastName){
+        return `Please write your full name!`
+    }
+
+    if(firstName.length < 2 || lastName.length < 2){
+        return `Length must be at least 2 symbols!`
+    }
+
+    return false;
+};
+
+
+function isValidBgPhoneNumber(phoneNumber) {
+    let cleaned = phoneNumber.replace(/[^\d+]/g, '');
+
+    const bulgarianPhoneRegex = /^(?:\+359|00359|0)(87|88|89|2\d{1})\d{7}$/;
+
+    if(bulgarianPhoneRegex.test(cleaned)){
+        return false;
+    }else{
+        return "Please write valid phone number!"
+    }
+};
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    if(emailRegex.test(email)){
+        return false;
+    }else{
+        return "Please write valid Email Address!"
+    }
+};
+
+
+
+const emailSender = (formElmemnt)=>{
     const SERVICEid = import.meta.env.VITE_SERVICE_ID;
     const TEMPLATEid = import.meta.env.VITE_TEMPLATE_ID;
     const PUBLICKKEY = import.meta.env.VITE_PUBLIC_KEY;
 
-    debugger
 
-    const handleFocus = (e)=>{
-        setSelectedInput(e.target.getAttribute('id'))
-    }
-
-    const handleBlur = ()=>{
-        setSelectedInput('')
-    }
-
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        
-        emailjs
-        .sendForm(SERVICEid, TEMPLATEid, formRef.current, {
+    emailjs
+        .sendForm(SERVICEid, TEMPLATEid, formElmemnt, {
           publicKey: PUBLICKKEY,
         })
         .then(
           () => {
-            console.log('SUCCESS!');
+            return true;
           },
           (error) => {
-            console.log('FAILED...', error.text);
+            return false;
           },
         );
+
+}
+
+
+
+
+export default function BookingForm(){
+    const [selectedInput, setSelectedInput] = useState('');
+    const [errors, setErrors] = useState({
+        email: false,
+        name: false,
+        phone: false,
+        additionalComments: false
+    })
+
+    const formRef = useRef();
+
+
+
+    
+
+    
+
+    const handleFocus = (e)=>{
+        setSelectedInput(e.target.getAttribute('id'))
+    };
+
+    const handleBlur = ()=>{
+        setSelectedInput('')
+    };
+
+    const errorHandlers = (email, name, phone)=>{
+
+        
+        const emailErr = isValidEmail(email);
+        const nameErr = checkName(name);
+        const phoneErr = isValidBgPhoneNumber(phone);
+
+
+        const errors = {
+            email: emailErr,
+            name:nameErr,
+            phone: phoneErr,
+        }
+        return errors;
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+
+        const fd = new FormData(formRef.current);
+        const data = Object.fromEntries(fd.entries());
+
+        const errorsFound = errorHandlers(data.user_email, data.user_name, data.phone)
+        
+        debugger
+        if(Object.values(errorsFound).some(e=> e != false)){
+            setErrors(errorsFound);
+            return;
+        };
+
+        const emailSended = emailSender(formRef.current);
+
+        if(!emailSended){
+            console.log("ASD");
+            
+        }
     }
 
 
@@ -41,6 +137,7 @@ export default function BookingForm(){
         <form ref={formRef} className="booking-form" onSubmit={handleSubmit}>
             <h1>Send Us Email</h1>
             <div className="inputEl">
+                {errors.name && <p>Error: {errors.name}</p>}
                 <label className={selectedInput === 'user_name' ? 'selectedLabel' : ''} htmlFor="user_name">Full Name:</label>
                 <input 
                     type="text" 
@@ -52,6 +149,7 @@ export default function BookingForm(){
                 />
             </div>
             <div className="inputEl">
+                {errors.email && <p>Error: {errors.email}</p>}  
                 <label className={selectedInput === 'user_email' ? 'selectedLabel' : ''} htmlFor="user_email">Email:</label>
                 <input 
                     type="email" 
@@ -63,6 +161,7 @@ export default function BookingForm(){
                 />
             </div>
             <div className="inputEl">
+                {errors.phone && <p>Error: {errors.phone}</p>}
                 <label className={selectedInput === 'phone' ? 'selectedLabel' : ''} htmlFor="phone">Phone Number:</label>
                 <input 
                     type="text" 
